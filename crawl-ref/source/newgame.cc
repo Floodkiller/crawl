2a59c0e838ec04e94bd32cb5e1e66148cfc0507a
tollymain <thbkwrm1@gmail.com>
2019-04-22 22:03:42 -0700

    added dwarrow

--- crawl-ref/source/newgame.cc
+++ crawl-ref/source/newgame.cc
@@ -182,6 +182,7 @@ static const species_type species_order[] =
     SP_DEEP_ELF,       SP_DEEP_DWARF,
     SP_HILL_ORC,       SP_SLUDGE_ELF,
     SP_PROMETHEAN,     SP_LAVA_ORC,
+    SP_DWARROW,
     // small species
     SP_HALFLING,       SP_KOBOLD,
     SP_SPRIGGAN,       SP_FAERIE_DRAGON,
